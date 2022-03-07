984627edfd185c1f96965acf9bf35466c61e3dfc
Erickromero789 <64759936+Erickromero789@users.noreply.github.com>
2022-03-07 15:36:42 -0500

    Update n_queens.pl

--- n_queens.pl
+++ n_queens.pl
@@ -1,16 +1,24 @@
-:- use_module(clpz).
+queens(N, Queens) :-
+    length(Queens, N),
+	board(Queens, Board, 0, N, _, _),
+	queens(Board, 0, Queens).
 
-n_queens(N, Qs) :-
-        length(Qs, N),
-        Qs ins 1..N,
-        safe_queens(Qs).
+board([], [], N, N, _, _).
+board([_|Queens], [Col-Vars|Board], Col0, N, [_|VR], VC) :-
+	Col is Col0+1,
+	functor(Vars, f, N),
+	constraints(N, Vars, VR, VC),
+	board(Queens, Board, Col, N, VR, [_|VC]).
 
-safe_queens([]).
-safe_queens([Q|Qs]) :- safe_queens(Qs, Q, 1), safe_queens(Qs).
+constraints(0, _, _, _) :- !.
+constraints(N, Row, [R|Rs], [C|Cs]) :-
+	arg(N, Row, R-C),
+	M is N-1,
+	constraints(M, Row, Rs, Cs).
 
-safe_queens([], _, _).
-safe_queens([Q|Qs], Q0, D0) :-
-        Q0 #\= Q,
-        abs(Q0 - Q) #\= D0,
-        D1 #= D0 + 1,
-        safe_queens(Qs, Q0, D1).
+queens([], _, []).
+queens([C|Cs], Row0, [Col|Solution]) :-
+	Row is Row0+1,
+	select(Col-Vars, [C|Cs], Board),
+	arg(Row, Vars, Row-Row),
+	queens(Board, Row, Solution).
