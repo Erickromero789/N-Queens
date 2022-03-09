181d21019d4c84ad221f65d15c52a9a898346fc5
Erickromero789 <64759936+Erickromero789@users.noreply.github.com>
2022-03-08 19:11:13 -0500

    Update n_queens.pl

--- n_queens.pl
+++ n_queens.pl
@@ -1,16 +1,16 @@
 :- use_rendering(chess).
 
-queens(N, Queens) :-
-    length(Queens, N),
-	board(Queens, Board, 0, N, _, _),
-	queens(Board, 0, Queens).
+reinas(N, Reinas) :-
+    length(Reinas, N),
+	board(Reinas, Board, 0, N, _, _),
+	reinas(Board, 0, Reinas).
 
 board([], [], N, N, _, _).
-board([_|Queens], [Col-Vars|Board], Col0, N, [_|VR], VC) :-
+board([_|Reinas], [Col-Vars|Board], Col0, N, [_|VR], VC) :-
 	Col is Col0+1,
 	functor(Vars, f, N),
 	constraints(N, Vars, VR, VC),
-	board(Queens, Board, Col, N, VR, [_|VC]).
+	board(Reinas, Board, Col, N, VR, [_|VC]).
 
 constraints(0, _, _, _) :- !.
 constraints(N, Row, [R|Rs], [C|Cs]) :-
@@ -18,9 +18,9 @@ constraints(N, Row, [R|Rs], [C|Cs]) :-
 	M is N-1,
 	constraints(M, Row, Rs, Cs).
 
-queens([], _, []).
-queens([C|Cs], Row0, [Col|Solution]) :-
+reinas([], _, []).
+reinas([C|Cs], Row0, [Col|Solution]) :-
 	Row is Row0+1,
 	select(Col-Vars, [C|Cs], Board),
 	arg(Row, Vars, Row-Row),
-	queens(Board, Row, Solution).
+	reinas(Board, Row, Solution).
